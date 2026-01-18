288e91e48b724d23268f03862cb2434d6d94b8fd
sdmule <mulesaurabh45@gmail.com>
2026-01-18 12:34:45 +0530

    Added Images and Update in action

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -38,15 +38,29 @@ namespace WhiteLagoon.Web.Controllers
             }
             return View();
         }
-
+        [HttpGet]
         public IActionResult Update(int villaId)
         {
             Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            //Villa? obj = _db.Villas.Find(villaId);
+            //var VillaList = _db.Villas.Where(u => u.Price > 50 && u.Occupancy > 0);     
             if(obj == null)
             {
-                return NotFound();
+                return RedirectToAction("Error", "Home");
             }
             return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id>0)
+            {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
