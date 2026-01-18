68bab725c474a8de40675e594c25408f2489d3bc
sdmule <mulesaurabh45@gmail.com>
2026-01-18 13:20:19 +0530

    Delete in Action

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -38,6 +38,7 @@ namespace WhiteLagoon.Web.Controllers
             }
             return View();
         }
+
         [HttpGet]
         public IActionResult Update(int villaId)
         {
@@ -57,6 +58,31 @@ namespace WhiteLagoon.Web.Controllers
             if (ModelState.IsValid && obj.Id>0)
             {
                 _db.Villas.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);   
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
