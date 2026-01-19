65e6bcc94473152db537c4700aa5252fed3ce542
sdmule <mulesaurabh45@gmail.com>
2026-01-19 17:21:48 +0530

    Added VillaNumber model, Created views for Create and Index actions

---
+++ WhiteLagoon.Infrastructure/Migrations/20260119112914_AddVillaTable.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVillaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VillaNumber_Villa_VillaId",
+                table: "VillaNumber");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_VillaNumber",
+                table: "VillaNumber");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Villa",
+                table: "Villa");
+
+            migrationBuilder.RenameTable(
+                name: "VillaNumber",
+                newName: "VillaNumbers");
+
+            migrationBuilder.RenameTable(
+                name: "Villa",
+                newName: "Villas");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_VillaNumber_VillaId",
+                table: "VillaNumbers",
+                newName: "IX_VillaNumbers_VillaId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_VillaNumbers",
+                table: "VillaNumbers",
+                column: "Villa_Number");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Villas",
+                table: "Villas",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VillaNumbers_Villas_VillaId",
+                table: "VillaNumbers",
+                column: "VillaId",
+                principalTable: "Villas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VillaNumbers_Villas_VillaId",
+                table: "VillaNumbers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Villas",
+                table: "Villas");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_VillaNumbers",
+                table: "VillaNumbers");
+
+            migrationBuilder.RenameTable(
+                name: "Villas",
+                newName: "Villa");
+
+            migrationBuilder.RenameTable(
+                name: "VillaNumbers",
+                newName: "VillaNumber");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_VillaNumbers_VillaId",
+                table: "VillaNumber",
+                newName: "IX_VillaNumber_VillaId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Villa",
+                table: "Villa",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_VillaNumber",
+                table: "VillaNumber",
+                column: "Villa_Number");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VillaNumber_Villa_VillaId",
+                table: "VillaNumber",
+                column: "VillaId",
+                principalTable: "Villa",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaNumberController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var villaNumbers = _db.VillaNumbers.ToList();
+            return View(villaNumbers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(VillaNumber obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.VillaNumbers.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The Villa Number has been created successfully.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            //Villa? obj = _db.Villas.Find(villaId);
+            //var VillaList = _db.Villas.Where(u => u.Price > 50 && u.Occupancy > 0);     
+            if(obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid && obj.Id>0)
+            {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been updated successfully.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);   
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                TempData["success"] = "The villa has been deleted successfully.";
+                return RedirectToAction("Index");
+            }
+            TempData["error"] = "The villa could not be deleted.";
+            return View();
+        }
+    }
+}
