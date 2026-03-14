8d468f2ec8b2f3f81e61ecd8a3b8191058e66f98
sdmule <mulesaurabh45@gmail.com>
2026-03-14 23:01:00 +0530

    Load Villa Amenity and Home Page UI

--- WhiteLagoon.Web/Controllers/HomeController.cs
+++ WhiteLagoon.Web/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace WhiteLagoon.Web.Controllers
         {
             HomeVM homeVM = new()
             {
-                VillaList = _unitOfWork.Villa.GetAll(),
+                VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity"),
                 Nights = 1,
                 CheckInDate = DateOnly.FromDateTime(DateTime.Now),
             };
