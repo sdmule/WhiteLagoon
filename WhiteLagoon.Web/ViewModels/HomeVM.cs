6eb378ea3137f72d6067197f38303c2f8c75a68f
sdmule <mulesaurabh45@gmail.com>
2026-03-14 22:41:26 +0530

    Created Home view Model

---
+++ WhiteLagoon.Web/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Web.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Villa>? VillaList { get; set; }
+        public DateOnly CheckInDate{ get; set; }
+        public DateOnly? CheckOutDate { get; set; }
+        public int Nights { get; set; }
+    }
+}
