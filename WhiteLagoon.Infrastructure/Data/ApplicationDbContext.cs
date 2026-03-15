d2165311aae4d75dd00134517af0ac7050df134f
sdmule <mulesaurabh45@gmail.com>
2026-03-15 19:48:37 +0530

    Created Identity tables

--- WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -15,7 +15,7 @@ namespace WhiteLagoon.Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //   base.OnModelCreating(modelBuilder);
+            base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Villa>().HasData(
                 new Villa
