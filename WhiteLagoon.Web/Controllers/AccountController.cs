0069d9331f9f878bfe0748a0af6f6df240d77f82
sdmule <mulesaurabh45@gmail.com>
2026-03-17 18:23:19 +0530

    Login Get Action

--- WhiteLagoon.Web/Controllers/AccountController.cs
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using WhiteLagoon.Application.Common.Interfaces;
 using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Web.ViewModels;
 
 namespace WhiteLagoon.Web.Controllers
 {
@@ -23,9 +24,17 @@ namespace WhiteLagoon.Web.Controllers
             _roleManager = roleManager;
         }
 
-        public IActionResult Login()
+        [HttpGet]
+        public IActionResult Login(string returnUrl=null)
         {
-            return View();
+            returnUrl ??= Url.Content("~/");
+
+            LoginVM loginVM = new()
+            {
+                RedirectUrl = returnUrl
+            };
+
+            return View(loginVM);
         }
         public IActionResult Register()
         {
