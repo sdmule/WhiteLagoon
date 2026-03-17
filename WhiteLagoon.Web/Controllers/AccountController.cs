ee9865f776580dbedfddba668d50d3b1890d24ca
sdmule <mulesaurabh45@gmail.com>
2026-03-17 20:27:26 +0530

    Retrieve Roles in Dropdown

--- WhiteLagoon.Web/Controllers/AccountController.cs
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using WhiteLagoon.Application.Common.Interfaces;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Web.ViewModels;
@@ -43,7 +44,17 @@ namespace WhiteLagoon.Web.Controllers
             _roleManager.CreateAsync(new IdentityRole("Admin")).Wait();
             _roleManager.CreateAsync(new IdentityRole("Customer")).Wait();
             }
-            return View();
+
+            RegisterVM registerVM = new()
+            {
+                RoleList = _roleManager.Roles.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Name
+                })
+            };
+
+            return View(registerVM);
         }
     }
 }
--- WhiteLagoon.Web/ViewModels/RegisterVM.cs
+++ WhiteLagoon.Web/ViewModels/RegisterVM.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace WhiteLagoon.Web.ViewModels
 {
@@ -23,5 +25,9 @@ namespace WhiteLagoon.Web.ViewModels
         [Display(Name ="Phone Number")]
         public string? PhoneNumber { get; set; }
         public string? RedirectUrl { get; set; }
+        public string? Role { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? RoleList { get; set; }
     }
 }
