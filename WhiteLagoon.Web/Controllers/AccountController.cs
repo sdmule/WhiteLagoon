8d14c286405ea2fc0255c18e76bd28d2efaddac4
sdmule <mulesaurabh45@gmail.com>
2026-03-21 12:44:48 +0530

    Access Denied UI

--- WhiteLagoon.Web/Controllers/AccountController.cs
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -46,6 +46,11 @@ namespace WhiteLagoon.Web.Controllers
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
         public IActionResult Register()
         {
             if (!_roleManager.RoleExistsAsync(SD.Role_Admin).GetAwaiter().GetResult())
