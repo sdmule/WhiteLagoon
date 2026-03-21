5e37aa97cc32f688f3ecc15ee12bf56cf2f4a947
sdmule <mulesaurabh45@gmail.com>
2026-03-21 23:08:34 +0530

    Moved Villa List to Partial View and reload partial view without refreshing page

--- WhiteLagoon.Web/Controllers/HomeController.cs
+++ WhiteLagoon.Web/Controllers/HomeController.cs
@@ -29,17 +29,29 @@ namespace WhiteLagoon.Web.Controllers
         public IActionResult Index(HomeVM homeVM)
         {
             homeVM.VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity");
-            foreach(var villa in homeVM.VillaList)
-            {
-                if(villa.Id % 2 == 0)
-                {
-                    villa.IsAvailable = false;
-                }
-            }
+            
                
             return View(homeVM);
         }
 
+        public IActionResult GetVillasByDate(int nights, DateOnly checkInDate)
+        {
+            var villaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").ToList();
+            foreach (var villa in villaList)
+            {
+                if (villa.Id % 2 == 0)
+                {
+                    villa.IsAvailable = false;
+                }
+            }
+            HomeVM homeVM = new()
+            {
+                CheckInDate = checkInDate,
+                VillaList = villaList,
+                Nights = nights
+            };
+            return PartialView("_VillaList",homeVM);
+        }
         public IActionResult Privacy()
         {
             return View();
