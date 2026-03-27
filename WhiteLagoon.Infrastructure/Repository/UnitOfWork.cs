d88f172ebaeca13efeef5c7ed5a52dbce747a486
sdmule <mulesaurabh45@gmail.com>
2026-03-27 18:07:21 +0530

    Created booking table and booking repository

--- WhiteLagoon.Infrastructure/Repository/UnitOfWork.cs
+++ WhiteLagoon.Infrastructure/Repository/UnitOfWork.cs
@@ -14,10 +14,12 @@ namespace WhiteLagoon.Infrastructure.Repository
         public IVillaRepository Villa { get; private set; }
         public IVillaNumberRepository VillaNumber { get; private set; }
         public IAmenityRepository Amenity { get; private set; }
+        public IBookingRepository Booking { get; private set; } 
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
             Villa = new VillaRepository(_db);
+            Booking = new BookingRepository(_db);
             VillaNumber = new VillaNumberRepository(_db);
             Amenity = new AmenityRepository(_db);
         }
