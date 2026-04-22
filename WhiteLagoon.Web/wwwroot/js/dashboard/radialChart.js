31bc389ae8849004da76713c6be41d1b38577291
sdmule <mulesaurabh45@gmail.com>
2026-04-22 17:56:18 +0530

    custom color for radial chart

--- WhiteLagoon.Web/wwwroot/js/dashboard/radialChart.js
+++ WhiteLagoon.Web/wwwroot/js/dashboard/radialChart.js
@@ -1,5 +1,9 @@
 ﻿function loadRadialBarChart(id, data) {
+    var chartColors = getChartColorsArray(id);
     var options = {
+        fill: {
+            colors: chartColors
+        },
         chart: {
             height: 90,
             width: 90,
@@ -27,4 +31,21 @@
 
     var chart = new ApexCharts(document.querySelector("#" + id), options);
     chart.render();
+}
+
+function getChartColorsArray(id) {
+    if (document.getElementById(id) != null) {
+        var colors = document.getElementById(id).getAttribute("data-colors");
+        if (colors) {
+            colors = JSON.parse(colors);
+            return colors.map(function (value) {
+                var newValue = value.replace(" ", "");
+                if (newValue.indexOf(",") === -1) {
+                    var color = getComputedStyle(document.documentElement).getPropertyValue(newValue);
+                    if (color) return color;
+                    else return newValue;;
+                }
+            });
+        }
+    }
 }
