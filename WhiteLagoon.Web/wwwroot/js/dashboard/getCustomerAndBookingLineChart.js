1dd85592e8512807f238a1d7e7f50ebb4743722c
sdmule <mulesaurabh45@gmail.com>
2026-04-23 12:05:35 +0530

    UI styling for Line Chart

---
+++ WhiteLagoon.Web/wwwroot/js/dashboard/getCustomerAndBookingLineChart.js
@@ -0,0 +1,70 @@
+﻿
+$(document).ready(function () {
+    loadCustomerAndBookingLineChart();
+});
+function loadCustomerAndBookingLineChart() {
+    $(".chart-spinner").show();
+
+    $.ajax({
+        url: "/Dashboard/GetMemberAndBookingLineChartData",
+        type: 'GET',
+        dataType: 'json',
+        success: function (data) {
+
+            loadLineChart("newMembersAndBookingsLineChart", data);
+
+            $(".chart-spinner").hide();
+        }
+    });
+}
+
+function loadLineChart(id, data) {
+    var chartColors = getChartColorsArray(id);
+
+    var options = {
+        series: data.series,
+        labels: data.labels,
+
+        chart: {
+            type: 'line',
+            height: 350
+        },
+        stroke: {
+            curve: 'smooth',
+            width: 2
+        },
+        markers: {
+            size: 3,
+            strokeWidth: 0,
+            hover: {
+                size: 7
+            }
+        },
+        xaxis: {
+            categories: data.categories,
+            labels: {
+                style: {
+                    colors: "#ddd"
+                }
+            }
+        },
+        yaxis: {
+            labels: {
+                style: {
+                    colors: "#fff"
+                }
+            }
+        },
+        legend: {
+            labels: {
+                colors: "#fff"
+            }
+        },
+        tooltip: {
+            theme: 'dark'
+        }
+    };
+
+    var chart = new ApexCharts(document.querySelector("#" + id), options);
+    chart.render();
+}
