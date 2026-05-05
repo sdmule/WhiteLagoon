4bbd071c4b0f2dac8a4524aba9536bf3e20e14ce
sdmule <mulesaurabh45@gmail.com>
2026-05-05 13:52:08 +0530

    added amenity list in export pdf

--- WhiteLagoon.Web/Controllers/HomeController.cs
+++ WhiteLagoon.Web/Controllers/HomeController.cs
@@ -95,6 +95,27 @@ namespace WhiteLagoon.Web.Controllers
                 shape.TextBody.Text = string.Format("USD {0}/night", villa.Price.ToString("c"));
             }
 
+            shape = slide.Shapes.FirstOrDefault(u => u.ShapeName == "txtVillaAmenitiesHeading") as IShape;
+            if (shape is not null)
+            {
+                List<string> listItems = villa.VillaAmenity.Select(x => x.Name).ToList();
+
+                shape.TextBody.Text = "";
+
+                foreach (var item in listItems)
+                {
+                    //shape.TextBody.Text += item + "\n";
+                    IParagraph paragraph = shape.TextBody.AddParagraph();
+                    ITextPart textPart = paragraph.AddTextPart(item);
+
+                    paragraph.ListFormat.Type = ListType.Bulleted;
+                    paragraph.ListFormat.BulletCharacter = '\u2022';
+                    textPart.Font.FontName = "system-ui";
+                    textPart.Font.FontSize = 18;
+                    textPart.Font.Color = ColorObject.FromArgb(144, 148, 152);
+                }
+            }
+
             MemoryStream memoryStream = new();
             presentation.Save(memoryStream);
             memoryStream.Position = 0;
