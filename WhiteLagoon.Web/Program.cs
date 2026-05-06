8594205c2fd8efd5321c02419584ff0dbcf81682
sdmule <mulesaurabh45@gmail.com>
2026-05-06 11:09:18 +0530

    DbInitializer setup

--- WhiteLagoon.Web/Program.cs
+++ WhiteLagoon.Web/Program.cs
@@ -51,9 +51,19 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+SeedDatabase();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<DbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
