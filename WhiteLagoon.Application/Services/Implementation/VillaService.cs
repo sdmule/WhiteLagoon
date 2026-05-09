70fc1f6d62db458c1a0e16067265c916c66fd9b5
sdmule <mulesaurabh45@gmail.com>
2026-05-09 10:18:42 +0530

    Home Controller with service

--- WhiteLagoon.Application/Services/Implementation/VillaService.cs
+++ WhiteLagoon.Application/Services/Implementation/VillaService.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Application.Common.Utility;
 using WhiteLagoon.Application.Services.Interface;
 using WhiteLagoon.Domain.Entities;
 
@@ -75,6 +76,23 @@ namespace WhiteLagoon.Application.Services.Implementation
         public Villa GetVillaById(int id)
         {
             return _unitOfWork.Villa.Get(u => u.Id == id, includeProperties: "Villa");
+        }
+
+        public IEnumerable<Villa> GetVillasAvailabilityByDate(int nights, DateOnly checkInDate)
+        {
+            var villaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity").ToList();
+            var villaNumbersList = _unitOfWork.VillaNumber.GetAll().ToList();
+            var bookedVillas = _unitOfWork.Booking.GetAll(u => u.Status == SD.StatusApproved ||
+                               u.Status == SD.StatusCheckedIn).ToList();
+
+            foreach (var villa in villaList)
+            {
+                int roomAvailable = SD.VillaRoomsAvailable_Count
+                    (villa.Id, villaNumbersList, checkInDate, nights, bookedVillas);
+
+                villa.IsAvailable = roomAvailable > 0 ? true : false;
+            }
+            return villaList;
         }
 
         public void UpdateVilla(Villa villa)
--- WhiteLagoon.Application/Services/Interface/IVillaService.cs
+++ WhiteLagoon.Application/Services/Interface/IVillaService.cs
@@ -14,5 +14,6 @@ namespace WhiteLagoon.Application.Services.Interface
         void CreateVilla(Villa villa);
         void UpdateVilla(Villa villa);
         bool DeleteVilla(int id);
+        IEnumerable<Villa> GetVillasAvailabilityByDate(int nights, DateOnly checkInDate);
     }
 }
