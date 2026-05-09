64ee8819061f5c99469749fd543d805f8ef86813
sdmule <mulesaurabh45@gmail.com>
2026-05-09 11:12:32 +0530

    Booking Interface

---
+++ WhiteLagoon.Application/Services/Interface/IBookingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Application.Services.Interface
+{
+    public interface IBookingService
+    {
+        void CreateBooking(Booking booking);
+        IEnumerable<Booking> GetAllBookings(string userId = "", string? statusFilterList = "");
+        Booking GetBookingById(int bookingId);
+    }
+}
